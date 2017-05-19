edfd4ede3bd414e19368e84cfa88986dd0bdd100
nfpirus <nfpirus@gmail.com>
2017-05-19 17:14:14 +0300

    demo build fix

--- grunt/config/ts.js
+++ grunt/config/ts.js
@@ -1,12 +1,22 @@
 module.exports = function(grunt, options) {
   return {
-	  default: {
-		tsconfig: true
-	  },
-	  demo: {
-		  src: ['demo/App.ts'],
-		  out: 'demo/demo.js',
-		  options: options.demoTsOptions
-	  }
+    default: {
+    tsconfig: true
+    },
+    demo: {
+      src: ['demo/App.ts'],
+      out: 'demo/demo.js',
+      options: {
+        sourceMap: true,
+        module: "system",
+        target: "es5",
+        moduleResolution: "node",
+        noImplicitAny: true,
+        noUnusedLocals: true,
+        noImplicitReturns: true,
+        failOnTypeErrors: true,
+        stripInternal: true
+      }
+    }
   }
 }
